fb5aa687e8dde05673b1f3705ab4f53d9fd74a76
nazishedureka <nazish.fraz@edureka.co>
2020-07-26 18:37:30 +0530

    remove api key

--- js/app.js
+++ js/app.js
@@ -143,11 +143,24 @@ $(document).ready(function(){
         })
     }
 
+    function initMap() {
+        const myLatLng = { lat: -25.363, lng: 131.044 };
+        const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: myLatLng
+        });
+        new google.maps.Marker({
+        position: myLatLng,
+        map,
+        title: "Hello World!"
+        });
+    }
+
     function init(){
         get_users_data();
         get_user_todos_data();
         get_user_on_map();
-        initMap();
+        initMap()
     }
 
 
